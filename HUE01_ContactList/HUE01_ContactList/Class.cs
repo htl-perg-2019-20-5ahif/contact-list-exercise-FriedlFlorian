8e2cc1d871bdae0b5331986b00f903b432c9ae88
Florian Friedl <20150066@htblaperg.onmicrosoft.com>
2019-10-02 14:36:40 +0200

    HUE01_Friedl

---
+++ HUE01_ContactList/HUE01_ContactList/Class.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HUE01_ContactList
+{
+    public class IContactList
+    {
+        int id;
+        String firstname;
+        String lastname;
+        String email;
+
+        public IContactList(int id, string firstname, string lastname, string email)
+        {
+            this.id = id;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.email = email;
+        }
+
+        public int Id { get; set; }
+        public String Firstname { get; set; }
+        public String Lastname { get; set; }
+        public String Email { get; set; }
+    }
+
+    
+}
---
+++ HUE01_ContactList/HUE01_ContactList/Controllers/ContactListController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HUE01_ContactList.Controllers
+{
+    [Route("api/contact-list")]
+    [ApiController]
+    public class ContactListController : ControllerBase
+    {
+        List<IContactList> contacts = new List<IContactList>();
+
+        // GET: api/ContactList
+        [HttpGet]
+        public IActionResult GetAllItems()
+        {
+            return Ok(contacts);
+        }
+
+        // GET: api/ContactList/5
+        [HttpGet("{firstname}", Name = "Get")]
+        //public IContactList GetContact(String firstname)
+        //{
+        //    for(int i=0; i<contacts.Count; i++)
+        //    {
+        //        if (contacts[i].Firstname.Equals(firstname))
+        //        {
+        //            return Ok(contacts[i]);
+        //        }
+        //    }
+        //    return BadRequest("Invalid Name");
+        //}
+
+        // POST: api/ContactList
+        [HttpPost]
+        public IActionResult AddContact([FromBody] IContactList contact)
+        {
+            this.contacts.Add(contact);
+            return CreatedAtRoute("GetSpecificItem", new { index = contacts.IndexOf(contact) }, contact);
+        }
+
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete]
+        [Route("{index}")]
+        public IActionResult DeleteItem(int index)
+        {
+            if (index >= 0 && index < contacts.Count)
+            {
+                contacts.RemoveAt(index);
+                return NoContent();
+            }
+
+            return BadRequest("Invalid index");
+        }
+    }
+}
